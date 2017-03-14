5fd6fa5f315fe70026ee71bd239e2da6405eab63
DH Kim <inerplat@gmail.com>
2017-03-14 18:53:51 +0900

    문제해결 2-1 moo게임 성공

--- 문제해결/과제/2주차/2-1 moo게임(5904).cpp
+++ 문제해결/과제/2주차/2-1 moo게임(5904).cpp
@@ -4,47 +4,33 @@ using namespace std;
 
 vector <int> v;
 vector <int> sum;
+char answer;
+
+void back(int k, int n)
+{
+	
+	if (sum[k] + 1 == n)
+	{
+		answer = 'm';
+		return;
+	}
+	else if (sum[k] + 1 < n && sum[k] + 1 + k + 2 >= n)
+	{
+		answer = 'o';
+		return;
+	}
+	else if (n - (sum[k] + k + 3) > 0) back(k - 1, n - (sum[k] + k + 3));
+	else back(k-1, n);
+}
 int main()
 {
 	int n;
 	int i = 0;
-	int s = 3;
-	int end;
-	int center = 3;
-	int v_size;
-	int sp;
 	cin >> n;
 	sum.push_back(0);
-	while (sum[i] < n)
-	{
-		++i;
-		sum.push_back(sum[i - 1] * 2 + i + 2);
-	}
-	sum.pop_back();
-
-	if (sum[sum.size() - 1] + 1 == n)
-	{
-		cout << 'm';
-		return 0;
-	}
-	end = sum[sum.size() - 1] + sum.size() + 3 ;
-	v.push_back(center);
-	while (end + s < n)
-	{
-		v_size = v.size();
-		if (s + end > n) break;
-		v.push_back(++center);
-		s += center;
-		for (i = 0; i < v_size; i++)
-		{
-			if (s + end > n) goto out;
-			v.push_back(v[i]);
-			s += v[i];
-		}
-	}
-out:
-	if (end + s == n || end==n) cout << 'm';
-	else cout << 'o';
+	while (sum[i++] < n) sum.push_back(sum[i - 1] * 2 + i + 2);
+	back(i-2, n);
+	cout << answer;
 	return 0;
 }
 
